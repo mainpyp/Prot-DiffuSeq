1f55d5b096ba6240a621392efd9108b928e32fa1
Adrian Henkel <henkel@rostlab.informatik.tu-muenchen.de>
2023-03-19 18:35:21 +0100

    Changed train file

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,15 +1,15 @@
 python -m torch.distributed.launch --nproc_per_node=4 --master_port=12233 --use_env run_train.py \
---diff_steps 2000 \
+--diff_steps 100 \
 --lr 0.0001 \
---learning_steps 50000 \
---save_interval 10000 \
+--learning_steps 500 \
+--save_interval 50 \
 --seed 102 \
 --noise_schedule sqrt \
 --hidden_dim 128 \
 --bsz 2048 \
---dataset qqp \
---data_dir {path-to-datasets} \
+--dataset ccs \
+--data_dir datasets/CommonsenseConversation \
 --vocab bert \
 --seq_len 128 \
 --schedule_sampler lossaware \
---notes test-qqp
+--notes test-ccs
