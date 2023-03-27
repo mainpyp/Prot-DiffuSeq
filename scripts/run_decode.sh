7cec84db9743aa37a51d78fcad87b882035eed3c
Adrian Henkel <henkeladrian@icloud.com>
2023-03-27 09:39:51 +0200

    Adapts run decode file

--- scripts/run_decode.sh
+++ scripts/run_decode.sh
@@ -1,4 +1,4 @@
 python -u run_decode.py \
---model_dir ../diffusion_models/diffuseq_ProtMedium_h128_lr0.001_t4000_sqrt_lossaware_seed102_test-ccs20230322-11\:41\:35/ \
+--model_dir /mnt/project/henkel/repositories/Prot-DiffuSeq/diffusion_models/diffuseq_ProtMediumCorrect_h128_lr0.001_t4000_sqrt_lossaware_seed102_MediumDatasetCorrectScope20230323-11:29:21 \
 --seed 123 \
 --split test
