02493ad42c2231de617d8e97c65e9af0d2be9ad6
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-03-27 17:00:59 +0200

    Adds model again?

--- diffuseq/gaussian_diffusion.py
+++ diffuseq/gaussian_diffusion.py
@@ -597,7 +597,7 @@ class GaussianDiffusion:
         assert 'input_ids' in model_kwargs
         input_ids_x = model_kwargs.pop('input_ids').to(t.device)
         input_ids_mask = model_kwargs.pop('input_mask').to(t.device)
-        x_start_mean = model.module.get_embeds(input_ids_x)
+        x_start_mean = model.model.module.get_embeds(input_ids_x)
         
         std = _extract_into_tensor(self.sqrt_one_minus_alphas_cumprod,
                                    th.tensor([0]).to(x_start_mean.device),
