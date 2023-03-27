649ccb8d78d2d1a3e118c86b107bc1c4a5709108
Adrian Henkel <henkeladrian@icloud.com>
2023-03-27 16:26:49 +0200

    Adds protbert and 3di tokens

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,15 +1,15 @@
 python -m torch.distributed.launch --nproc_per_node=2 --master_port=12233 --use_env run_train.py \
 --diff_steps 4000 \
---lr 0.001 \
---learning_steps 3000 \
---save_interval 50 \
+--lr 0.0001 \
+--learning_steps 5001 \
+--save_interval 1000 \
 --seed 102 \
 --noise_schedule sqrt \
 --hidden_dim 128 \
 --bsz 256 \
 --dataset ProtMedium \
---data_dir datasets/ProtTotal \
---vocab bert \
+--data_dir datasets/ProtMediumCorrect \
+--vocab protbert \
 --seq_len 128 \
 --schedule_sampler lossaware \
 --notes test-ccs
