2e33d44d5d44ae044af4beae6399557c0070cc9c
Adrian Henkel <henkeladrian@icloud.com>
2023-03-27 14:38:06 +0200

    Adds small train

---
+++ scripts/small_train.sh
@@ -0,0 +1,15 @@
+python -m torch.distributed.launch --nproc_per_node=2 --master_port=12233 --use_env run_train.py \
+--diff_steps 2000 \
+--lr 0.001 \
+--learning_steps 1001 \
+--save_interval 500 \
+--seed 102 \
+--noise_schedule sqrt \
+--hidden_dim 128 \
+--bsz 32 \
+--dataset ProtSmall \
+--data_dir datasets/ProtSmall \
+--vocab protbert \
+--seq_len 128 \
+--schedule_sampler lossaware \
+--notes Only100TrainingData
