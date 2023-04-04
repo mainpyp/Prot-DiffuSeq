dae5c29a3b9b95f4e4bf5e6a1f83acf8e45187c3
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-04-04 17:34:51 +0200

    Create medium_run_decode.sh

---
+++ scripts/medium_run_decode.sh
@@ -0,0 +1,4 @@
+python -u run_decode.py \
+--model_dir /mnt/project/henkel/repositories/Prot-DiffuSeq/diffusion_models/diffuseq_ProtSmall_h128_lr0.001_t2000_sqrt_lossaware_seed102_Only100TrainingData20230327-16:06:58/ \
+--seed 123 \
+--split test
