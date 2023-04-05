0f29918f6c21695750c81731dd6548d469bbcb97
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-04-05 17:38:18 +0200

    global fix

--- diffuseq/gaussian_diffusion.py
+++ diffuseq/gaussian_diffusion.py
@@ -598,7 +598,10 @@ class GaussianDiffusion:
         input_ids_x = model_kwargs.pop('input_ids').to(t.device) # shape: bsz, seqlen
         input_ids_mask = model_kwargs.pop('input_mask').to(t.device)
         x_start_mean = model.model.module.get_embeds(input_ids_x)
+        
+        global TOKENIZER
         TOKENIZER.decode(input_ids_x)
+        
         std = _extract_into_tensor(self.sqrt_one_minus_alphas_cumprod,
                                    th.tensor([0]).to(x_start_mean.device),
                                    x_start_mean.shape)
