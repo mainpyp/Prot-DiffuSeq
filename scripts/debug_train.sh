8b6afee48291e07ac331714304fa060a3687a21a
Adrian Henkel <henkeladrian@icloud.com>
2023-05-03 15:36:52 +0200

    Final debug train file

--- scripts/debug_train.sh
+++ scripts/debug_train.sh
@@ -1,15 +1,15 @@
-python -m torch.distributed.launch --nproc_per_node=2 --master_port=12233 --use_env run_train.py \
+python -m torch.distributed.launch --nproc_per_node=3 --master_port=12233 --use_env run_train.py \
 --diff_steps 2000 \
 --lr 0.0001 \
---learning_steps 5001 \
---save_interval 100 \
---seed 123 \
+--learning_steps 70000 \
+--save_interval 10000 \
+--seed 102 \
 --noise_schedule sqrt \
 --hidden_dim 128 \
---bsz 512 \
+--bsz 2048 \
 --dataset Conversation \
 --data_dir datasets/Conversation \
 --vocab bert \
---seq_len 256 \
+--seq_len 128 \
 --schedule_sampler lossaware \
---notes debug
+--notes debug-large
