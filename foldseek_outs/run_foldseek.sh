87eb51ee766ea04954e3f5d0058318695c319446
Adrian Henkel <henkeladrian@icloud.com>
2023-05-08 17:11:47 +0200

    Adds foldseek outs

---
+++ foldseek_outs/run_foldseek.sh
@@ -0,0 +1,6 @@
+/mnt/home/mheinzinger/deepppi1tb/foldseek/foldseek_latest/foldseek/bin/foldseek easy-search \
+    /mnt/home/mheinzinger/deepppi1tb/ProSST5/analysis_prosst5/reference_structures/AFDB/val/ \
+     ../esm_outs/ref/ \
+    ref/aln.m8 \
+    ref/tmp --format-output "query,target,pident,evalue,bits,alntmscore,lddt" \
+    --exhaustive-search 1
