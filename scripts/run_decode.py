eb15af82cb73342fd9e5caf5747a6531b552a406
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-05-12 17:07:22 +0200

    change

--- scripts/run_decode.py
+++ scripts/run_decode.py
@@ -91,10 +91,11 @@ if __name__ == '__main__':
     #### GET ALL GENERATED FILES ####
     for lst in glob.glob(args.model_dir):
         checkpoints = sorted(glob.glob(f"{lst}/{args.pattern}*.pt"))[::-1]
-        print(checkpoints)
-        split = lambda x: "/".join(x.split('/')[-3:]).replace(".pt", "/").replace("diffusion_models", out_dir)
-        generations = [split(ckpt) for ckpt in checkpoints]
-        print(generations)
+        parse_path = lambda x: "/".join(x.split('/')[-3:]).replace(".pt", ".pt.samples/").replace("diffusion_models", out_dir)
+        generations = [parse_path(ckpt) for ckpt in checkpoints]
+        sorted(glob.glob(f"{generations[0]}*.pt"))[::-1]
+
+
     #### CONVERT GENERATED JSON TO FASTA ####
     all_generated_fastas = convert_to_fasta(all_generated_files)
 
