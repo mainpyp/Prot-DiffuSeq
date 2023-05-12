074133a5cab4df3597c207a9c040dc2e4c06ddc6
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-05-12 15:56:55 +0200

    Mods run decode

--- scripts/run_decode.py
+++ scripts/run_decode.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='decoding args.')
     parser.add_argument('--model_dir', type=str, default='', help='path to the folder of diffusion model')
-    parser.add_argument('--seed', type=int, default=101, help='random seed')
+    parser.add_argument('--n_gpus', type=int, default=1, help='number of gpus')
+    parser.add_argument('--seeds', type=list, default=[101], help='random seed')
     parser.add_argument('--step', type=int, default=2000, help='if less than diffusion training steps, like 1000, use ddim sampling')
 
     parser.add_argument('--bsz', type=int, default=50, help='batch size')
@@ -31,16 +32,20 @@ if __name__ == '__main__':
 
         out_dir = 'generation_outputs'
         if not os.path.isdir(out_dir):
+            print(f'creating {out_dir}...')
             os.mkdir(out_dir)
+
 
         for checkpoint_one in checkpoints:
 
-            COMMAND = f'python -m torch.distributed.launch --nproc_per_node=1 --master_port={12233 + int(args.seed)} --use_env sample_seq2seq.py ' \
-            f'--model_path {checkpoint_one} --step {args.step} ' \
-            f'--batch_size {args.bsz} --seed2 {args.seed} --split {args.split} ' \
-            f'--out_dir {out_dir} --top_p {args.top_p} '
-            print(COMMAND)
+            assert os.path.isfile(checkpoint_one), f'{checkpoint_one} not found'
+
+            for seed in args.seeds:
+                COMMAND = f'python -m torch.distributed.launch --nproc_per_node={args.n_gpus} --master_port={12233 + int(seed)} --use_env sample_seq2seq.py ' \
+                f'--model_path {checkpoint_one} --step {args.step} ' \
+                f'--batch_size {args.bsz} --seed2 {seed} --split {args.split} ' \
+                f'--out_dir {out_dir} --top_p {args.top_p} '
+                print("Running: ", COMMAND)
             
             os.system(COMMAND)
-    
     print('#'*30, 'decoding finished...')
