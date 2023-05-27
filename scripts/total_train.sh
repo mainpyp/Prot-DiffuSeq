00be4c52070902aec4fe230669d23167524d1490
Adrian Henkel <henkeladrian@icloud.com>
2023-05-27 12:11:53 +0200

    Adds total train

---
+++ scripts/total_train.sh
@@ -0,0 +1,15 @@
+python -m torch.distributed.launch --nproc_per_node=2 --master_port=12233 --use_env run_train.py \
+--diff_steps 4000 \
+--lr 0.0001 \
+--learning_steps 5001 \
+--save_interval 1000 \
+--seed 102 \
+--noise_schedule sqrt \
+--hidden_dim 128 \
+--bsz 256 \
+--dataset ProtTotal \
+--data_dir datasets/ProtTotalCorrect \
+--vocab protbert \
+--seq_len 128 \
+--schedule_sampler lossaware \
+--notes ProtTotalFirstRealRun
