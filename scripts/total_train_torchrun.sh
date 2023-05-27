9e44f2a8d8de864c5dcc545d31f58bbe503a64f3
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-05-27 14:35:33 +0200

    Adds torchrun

---
+++ scripts/total_train_torchrun.sh
@@ -0,0 +1,15 @@
+torchrun --nnodes=1 --nproc_per_node=8 run_train.py  --master_port=12233   \
+--diff_steps 4000 \
+--lr 0.0001 \
+--learning_steps 5001 \
+--save_interval 1000 \
+--seed 102 \
+--noise_schedule sqrt \
+--hidden_dim 128 \
+--bsz 256 \
+--dataset ProtTotal \
+--data_dir datasets/ProtTotalCorrect \
+--vocab protbert \
+--seq_len 128 \
+--schedule_sampler lossaware \
+--notes ProtTotalFirstRealRun
