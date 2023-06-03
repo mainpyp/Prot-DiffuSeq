b6e5d3f6a739f4d150e58c3d0d84e72f6919f61a
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-06-03 14:23:53 +0200

    Update train_util.py

--- train_util.py
+++ train_util.py
@@ -140,12 +140,11 @@ class TrainLoop:
                 logger.log(f"loading model from checkpoint: {resume_checkpoint}...")
                 self.model.load_state_dict(
                     dist_util.load_state_dict(
-                        actual_model_path(resume_checkpoint)
+                        actual_model_path(resume_checkpoint), map_location=dist_util.dev()
                     )
                 )
 
         dist_util.sync_params(self.model.parameters())
-        
 
     def _load_ema_parameters(self, rate):
         ema_params = copy.deepcopy(self.master_params)
