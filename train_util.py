632540c9982721b028ad2805df94443e82bac2a2
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-06-03 15:06:12 +0200

    Removes state loading

--- train_util.py
+++ train_util.py
@@ -87,7 +87,7 @@ class TrainLoop:
 
         self.checkpoint_path = checkpoint_path # DEBUG **
 
-        self._load_and_sync_parameters()
+        # self._load_and_sync_parameters()
         if self.use_fp16:
             self._setup_fp16()
 
